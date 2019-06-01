79fe66a91183ebef9d530d262c1b3880debfbaff
Kovah <mail@kovah.de>
2019-06-01 12:34:52 +0200

    Implement footer styles, add social block

--- content/themes/kvh/assets/js/app.js
+++ content/themes/kvh/assets/js/app.js
@@ -1,11 +1,19 @@
 import { dom, library } from '@fortawesome/fontawesome-svg-core';
 import { faRss } from '@fortawesome/pro-light-svg-icons/faRss';
 import { faTwitter } from '@fortawesome/free-brands-svg-icons/faTwitter';
+import { faGithub } from '@fortawesome/free-brands-svg-icons/faGithub';
+import { faGlobe } from '@fortawesome/pro-light-svg-icons/faGlobe';
+import { faLinkedinIn } from '@fortawesome/free-brands-svg-icons/faLinkedinIn';
+import { faXing } from '@fortawesome/free-brands-svg-icons/faXing';
 
 window.onload = function () {
   // Font Awesome
   library.add(faRss);
+  library.add(faGlobe);
   library.add(faTwitter);
+  library.add(faGithub);
+  library.add(faLinkedinIn);
+  library.add(faXing);
 
   dom.i2svg();
 };
