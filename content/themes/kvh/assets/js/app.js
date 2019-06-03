8faee0784a6c0f9b04b8b717510f743e804f4da8
Kovah <mail@kovah.de>
2019-06-03 19:46:41 +0200

    Implement the pagination

--- content/themes/kvh/assets/js/app.js
+++ content/themes/kvh/assets/js/app.js
@@ -5,11 +5,15 @@ import { faGithub } from '@fortawesome/free-brands-svg-icons/faGithub';
 import { faGlobe } from '@fortawesome/pro-light-svg-icons/faGlobe';
 import { faLinkedinIn } from '@fortawesome/free-brands-svg-icons/faLinkedinIn';
 import { faXing } from '@fortawesome/free-brands-svg-icons/faXing';
+import { faArrowLeft } from '@fortawesome/pro-light-svg-icons/faArrowLeft';
+import { faArrowRight } from '@fortawesome/pro-light-svg-icons/faArrowRight';
 
 window.onload = function () {
   // Font Awesome
   library.add(faRss);
   library.add(faGlobe);
+  library.add(faArrowLeft);
+  library.add(faArrowRight);
   library.add(faTwitter);
   library.add(faGithub);
   library.add(faLinkedinIn);
