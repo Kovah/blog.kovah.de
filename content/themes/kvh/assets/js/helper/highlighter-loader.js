4dd51334edcf1306c1a21e5d44fa43d3a9f9e4f7
Kovah <mail@kovah.de>
2019-06-12 10:43:49 +0200

    Take a clean start

--- content/themes/kvh/assets/js/helper/highlighter-loader.js
+++
@@ -1,8 +0,0 @@
-export default function loadSyntaxHighlighter(){
-  const $post = document.querySelector('article.post pre');
-
-  if ($post !== null) {
-    const $highlightScript = document.getElementById('highlight-script');
-    $highlightScript.src = $highlightScript.dataset.src;
-  }
-}
